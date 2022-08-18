ae6303100ee7704e0edcf3af68a6f7be8955aab0
Jesus Herrera <heisusin@gmail.com>
2022-08-18 21:40:55 +0200

    feat: :sparkles: Created Navbar with links to another pages

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Navbar.css';
+import logoNav from '../../assets/images/logo-nav.svg';
+import { Link, Outlet } from 'react-router-dom';
+
+const Navbar = () => {
+	return (
+    <>
+		<nav className='nav__container'>
+			<Link to='/mainstage'>
+				<img src={logoNav} alt='navbar logo' className='nav__logo' />
+			</Link>
+			<div className='nav__menu--container'>
+				<Link className='nav__menu--link' to='/faq'>FAQ</Link>
+				<Link className='nav__menu--link' to='/contact'>Contact Us</Link>
+				<Link className='nav__menu--link' to='/login'>Login</Link>
+			</div>
+		</nav>
+    <section>
+      <Outlet />
+    </section>
+    </>
+	);
+};
+
+export default Navbar;
