3db1f4936b846a3bd50874952eb80c9a2d784686
Jesus Herrera <heisusin@gmail.com>
2022-08-22 01:16:12 +0200

    style: :lipstick: Created button with 'Styled-components'

---
+++ src/ui/OrderBtn/OrderBtn.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const OrderBtn = styled.button`
+	height: 1.9rem;
+	width: 6rem;
+	margin-left: 1rem;
+	background-color: #2bb0c7;
+	border: none;
+	border-radius: 0.3rem;
+	font-family: var(--project-font);
+	font-size: 0.8rem;
+	font-weight: 300;
+	letter-spacing: 0.1rem;
+	color: #fff;
+
+	&:hover {
+		border: none;
+		box-shadow: 0 0 20px #11dafd91;
+	}
+`;
