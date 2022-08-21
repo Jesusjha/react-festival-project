169597e80063e300b1f5e02311d1cb4a0649859c
Jesus Herrera <heisusin@gmail.com>
2022-08-22 01:17:54 +0200

    feat: :sparkles: Created 'Cart' component

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Cart.css';
+
+const Cart = () => {
+  return (
+    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim omnis harum maiores sint, illo repudiandae culpa porro fuga beatae neque necessitatibus vero numquam.</div>
+  )
+}
+
+export default Cart
