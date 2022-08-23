9891f3376681cea36e0484729e230a8c5d9fd20d
Jesus Herrera <heisusin@gmail.com>
2022-08-24 01:13:23 +0200

    style: :lipstick: Created cart design

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import Scrollbars from 'react-custom-scrollbars-2';
+import { Link } from 'react-router-dom';
 import './Cart.css';
 
 const Cart = () => {
   return (
-    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim omnis harum maiores sint, illo repudiandae culpa porro fuga beatae neque necessitatibus vero numquam.</div>
+    <main className="cart__container">
+      <p className="cart__title--p">YOUR PURCHASE</p>
+      <section className="cart__purchase--items"></section>
+      <section className="cart__price--container">
+        <article className="cart__price--money">
+          <p>100€</p>
+          <p>Tax: 21%</p>
+          <p>TOTAL: 100€</p>
+        </article>
+        <article className="cart__checkout--container">
+          <Link to='/checkout'>
+            <button className="cart__checkout--btn" type='button'>CHECKOUT</button>
+          </Link>
+        </article>
+      </section>
+    </main>
   )
 }
 
