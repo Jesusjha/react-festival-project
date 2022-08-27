9c6bc36b8d389571760ebf579f8645086e92c3b8
Jesus Herrera <heisusin@gmail.com>
2022-08-27 02:43:11 +0200

    feat: :sparkles: Created 404 - Not Found page

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import './NotFound.css'
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
   return (
-    <div>404 - Not Found</div>
+    <main className='notfound__bg'>
+      <section className='notfound__container'>
+        <article className='notfound_title--article'>
+          <p className="notfound__title">Oh no!</p>
+        </article>
+          <p className="notfound__number">404</p>
+        <article className="notfound__subtitle--article">
+          <p className="notfound__subtitle">You are totally lost in an imaginary festival!</p>
+        </article>
+        <Link to='/mainstage'><button className='notfound__btn'>TAKE ME HOME</button></Link>
+      </section>
+    </main>
   )
 }
 
