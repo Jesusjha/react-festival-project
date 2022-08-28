5959f38ce00af61cfbccb4ce551f118783b910ed
Jesus Herrera <heisusin@gmail.com>
2022-08-28 22:54:54 +0200

    perf: :zap: Finalized TicketDetail page and integrated Cart component

--- src/pages/Mainstage/components/Ticket/TicketDetail/TicketDetail.jsx
+++ src/pages/Mainstage/components/Ticket/TicketDetail/TicketDetail.jsx
@@ -1,45 +1,63 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import ReactPlayer from 'react-player/youtube';
 import { useParams } from 'react-router-dom';
+import Cart from '../../../../../components/Cart/Cart';
+import CartContext from '../../../../../context/CartContext/CartContext';
+import { BlueBtn } from '../../../../../ui/BlueBtn/BlueBtn';
+import './TicketDetail.css';
 
 const TicketDetail = () => {
-  
-/* Destructuring the festDetail from the useParams hook. */
-  const [detail, setDetail] = useState({});
+	/* Destructuring the festDetail from the useParams hook. */
+	const [detail, setDetail] = useState({});
+
 	const params = useParams();
 
-  const {title, image, price, video} = detail;
-  
-  
-  useEffect(() => {
-    getDetail()
-  },[]);
+	const { addTicket } = useContext(CartContext);
 
-  const urlDetail = `http://localhost:3333/tickets/${params.festDetail}`;
+	const { id, title, price, video, info } = detail;
+
+	useEffect(() => {
+		getDetail();
+	}, []);
+
+	const urlDetail = `http://localhost:3333/tickets/${params.festDetail}`;
 
 	const getDetail = async () => {
-
 		try {
 			const response = await fetch(urlDetail);
 			const result = await response.json();
 			setDetail(result);
-      console.log(result)
-
 		} catch (error) {
 			console.log(error);
 		}
 	};
 
-
 	return (
-		<>
-      <h1 className="detail__title">{title}</h1>
-			<div>
-      <ReactPlayer url={`https://www.youtube.com/embed/${video}`} controls={true} />
-    </div>
-		</>
+		<main className='main__detail--container'>
+			<h2 className='detail__title--h2'>{title}</h2>
+			<section className='detail__info--section'>
+				<section className='detail__info--container'>
+					<ReactPlayer
+						url={`https://www.youtube.com/embed/${video}`}
+						controls={true}
+						width={1121}
+						height={468}
+					/>
+					<article className='detail__info--checkout'>
+						<article>
+						<h4 className="detail__subtitle--span">Why should you go to {title}?</h4>
+							<p className='detail__info--p'>{info}</p>
+					<article className='order__container'>
+						<p className='detail__price--p'>{price}.00€</p>
+						<BlueBtn onClick={() => addTicket(id, title, price)} className='detail__order--btn'><i className='bx bxs-shopping-bag'></i> ORDER</BlueBtn>
+					</article>
+						</article>
+						<Cart className='detail__cart' />
+					</article>
+				</section>
+			</section>
+		</main>
 	);
 };
 
 export default TicketDetail;
-/* <ReactPlayer url={`https://www.youtube.com/embed/${video}`}/>  https://youtu.be/hEGNMnbcq6c   <iframe width="1280" height="720" src="https://www.youtube.com/embed/hEGNMnbcq6c" title="Tomorrowland 2022 THe AfterMovie" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>*/
