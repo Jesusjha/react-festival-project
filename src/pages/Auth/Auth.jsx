e227ea1d1610a7a11e7501ffc3cdb1fb15057b04
Jesus Herrera <heisusin@gmail.com>
2022-08-28 22:58:33 +0200

    perf: :zap: Finalized Auth(login) component and remove residual code from CSS

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -1,38 +1,52 @@
 import { useContext, useState } from 'react';
 import { AuthContext } from '../../context/AuthContext/AuthContext';
+import { BlueBtn } from '../../ui/BlueBtn/BlueBtn';
+import { GenericInput } from '../../ui/GenericInput/GenericInput';
 import './Auth.css';
+
 
 const Auth = () => {
   
-  const {Login, user, Logout, password} = useContext(AuthContext)
+  const {Login, user, Logout, password, error} = useContext(AuthContext)
   
 	const [details, setDetails] = useState({name:'', email:'', password:''});
   console.log(details);
 
+  // const getEmail = localStorage.getItem(emailData)
+  // const getPassword = localStorage.getItem(passwordData)
+
 	const submitHandler = (e) => {
 		e.preventDefault();
-  
+    
+    // if(details.email === 'jesus@mail.com' && details.password === 'jesus123') {
+    //   localStorage.setItem('emailData', details.email)
+    //   localStorage.setItem('password', details.password)
+    // }
+
 		Login(details);
 	}
 
 	return (
 		<form onSubmit={submitHandler}>
-			<div className='form-inner'>
-				<h2>Login</h2>
-				{/* ERROR! */}
-				<div className='form-group'>
-					<label htmlFor='name'>Name:</label>
-					<input
+			<div className='login__container'>
+				<h2 className='login__title--label'>Login</h2>
+				{(error != '') ? (<div className='login__error'>{error}</div>) : '' }
+				<div className='login__data'>
+					<label className='login__label' htmlFor='name'>Name*</label>
+					<GenericInput
+					className='login__input'
 						type='text'
 						name='name'
 						id='name'
 						onChange={(e) => setDetails({ ...details, name: e.target.value })}
 						value={details.name}
+						autoFocus
 					/>
 				</div>
-				<div className='form-group'>
-					<label htmlFor='email'>Email:</label>
-					<input
+				<div className='login__data'>
+					<label className='login__label' htmlFor='email'>Email*</label>
+					<GenericInput
+					className='login__input'
 						type='email'
 						name='email'
 						id='email'
@@ -40,9 +54,10 @@ const Auth = () => {
 						value={details.email}
 					/>
 				</div>
-				<div className='form-group'>
-					<label htmlFor='password'>Password:</label>
-					<input
+				<div className='login__data'>
+					<label className='login__label' htmlFor='password'>Password*</label>
+					<GenericInput
+					className='login__input'
 						type='password'
 						name='password'
 						id='password'
@@ -52,7 +67,7 @@ const Auth = () => {
 						value={details.password}
 					/>
 				</div>
-				<button type='submit'>LOGIN</button>
+				<BlueBtn styleLog type='submit'>LOGIN</BlueBtn>
 			</div>
       <p className='p'>{user.name}</p>
 		</form>
