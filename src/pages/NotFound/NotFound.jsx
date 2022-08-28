69dad6cd299f7222040212bc631363611ab28a08
Jesus Herrera <heisusin@gmail.com>
2022-08-28 22:53:11 +0200

    style: :lipstick: Added some styles and added trend Logo

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import './NotFound.css'
 import { Link } from 'react-router-dom';
+import logo404 from '../../assets/images/logo-bg.svg'
+import { BlueBtn } from '../../ui/BlueBtn/BlueBtn';
 
 const NotFound = () => {
   return (
     <main className='notfound__bg'>
       <section className='notfound__container'>
+      <img src={logo404} alt="" className="notfound__logo" />
         <article className='notfound_title--article'>
           <p className="notfound__title">Oh no!</p>
         </article>
@@ -13,7 +16,7 @@ const NotFound = () => {
         <article className="notfound__subtitle--article">
           <p className="notfound__subtitle">You are totally lost in an imaginary festival!</p>
         </article>
-        <Link to='/mainstage'><button className='notfound__btn'>TAKE ME HOME</button></Link>
+        <Link to='/mainstage'><BlueBtn styleLog className='notfound__btn'>TAKE ME HOME</BlueBtn></Link>
       </section>
     </main>
   )
